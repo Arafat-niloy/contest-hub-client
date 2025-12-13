d2aceebf146226ff67a9834049593ba780a6c657
Arafatul Islam <https://github.com/Arafat-niloy>
2025-12-14 01:39:33 +0600

    Sign Page added

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,95 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import toast from "react-hot-toast";
+
+const SignUp = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile, googleSignIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onSubmit = data => {
+        createUser(data.email, data.password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                        console.log('user profile info updated');
+                        reset();
+                        toast.success('User Created Successfully');
+                        navigate('/');
+                    })
+                    .catch(error => console.log(error))
+            })
+            .catch(error => {
+                console.error(error);
+                toast.error(error.message);
+            })
+    };
+
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                console.log(result.user);
+                toast.success('Google Login Successful');
+                navigate('/');
+            })
+            .catch(error => console.error(error));
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign up now!</h1>
+                    <p className="py-6">Join ContestHub today!</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+                            {errors.name && <span className="text-red-600">Name is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                            {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                            {errors.email && <span className="text-red-600">Email is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" {...register("password", { required: true, minLength: 6, maxLength: 20 })} name="password" placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                            {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                    <div className="p-4 pt-0">
+                        <div className="divider">OR</div>
+                        <button onClick={handleGoogleSignIn} className="btn btn-outline w-full uppercase">Google Sign In</button>
+                        <p className="mt-4 text-center"><small>Already have an account? <Link to="/login" className="text-orange-600 font-bold">Login</Link></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
